83da022a20c10568679294a32c736123ad694b6f
Pedro Gonçalves <pedro.goncalves@paytrack.com.br>
2023-12-05 18:11:16 -0300

    Improving list visualization

--- app.vue
+++ app.vue
@@ -11,11 +11,30 @@
       @click="addNewField"
     />
 
-    <div class="grid grid-cols-4 gap-4 my-2 pl-4 pr-16">
+    <UCheckbox v-model="useValueModifier">
+      <template #label>
+        <span class="italic">Apply a modifier</span>
+      </template>
+    </UCheckbox>
+
+    <div class="grid grid-cols-3 gap-3 my-2 pl-4 pr-16 items-center">
       <p>Name</p>
-      <p>Value</p>
-      <p>Multiplier</p>
-      <p>Product</p>
+      <div :class="{ 'grid grid-cols-2 gap-4 items-center': useValueModifier }">
+        <p>Value</p>
+        <USelectMenu
+          v-if="useValueModifier"
+          class="inline-block"
+          v-model="selectedModifierType"
+          :options="modifierTypes"
+          @change="recalculateAllProducts"
+        >
+          <template #option="{ option: valueModifierType }">
+            <UIcon :name="valueModifierType.icon" />
+            <span>{{ valueModifierType.label }}</span>
+          </template>
+        </USelectMenu>
+      </div>
+      <p v-if="useValueModifier">Product</p>
     </div>
 
     <Draggable
@@ -28,21 +47,49 @@
           :id="element.id"
           class="flex flex-row justify-start gap-4 items-center py-2 px-4 bg-slate-800 rounded-xl mb-4"
         >
-          <div class="w-full grid grid-cols-4 gap-4 my-2">
+          <div class="w-full grid grid-cols-3 gap-4 my-2">
             <UInput placeholder="Name" v-model="element.name" />
+
+            <div :class="{ 'grid grid-cols-2 gap-4': useValueModifier }">
+              <UInput
+                placeholder="Value"
+                v-model="element.value"
+                type="number"
+                @change="calculateProduct(index)"
+              />
+              <UInput
+                v-if="
+                  useValueModifier &&
+                  selectedModifierType.label === 'Multiplier'
+                "
+                :placeholder="selectedModifierType.label"
+                v-model="element.multiplier"
+                type="number"
+                @change="calculateProduct(index)"
+                ><template #leading>
+                  <UIcon :name="selectedModifierType.icon" dynamic />
+                </template>
+              </UInput>
+              <UInput
+                v-if="
+                  useValueModifier &&
+                  selectedModifierType.label === 'Percentage'
+                "
+                placeholder="Percentage"
+                v-model="element.multiplier"
+                type="number"
+                min="0"
+                max="100"
+                step="1"
+                @change="calculateProduct(index)"
+              >
+                <template #leading>
+                  <UIcon :name="selectedModifierType.icon" dynamic />
+                </template>
+              </UInput>
+            </div>
             <UInput
-              placeholder="Value"
-              v-model="element.value"
-              type="number"
-              @change="calculateProduct(index)"
-            />
-            <UInput
-              placeholder="Multiplier"
-              v-model="element.multiplier"
-              type="number"
-              @change="calculateProduct(index)"
-            />
-            <UInput
+              v-if="useValueModifier"
               placeholder="Product"
               v-model="element.product"
               readonly
@@ -63,8 +110,8 @@
       </template>
     </Draggable>
 
-    <div class="grid grid-cols-4 gap-4 my-2 pl-4 pr-16">
-      <div class="col-start-4 grid grid-cols-2 gap-4">
+    <div class="grid grid-cols-3 gap-4 my-2 pl-4 pr-16">
+      <div class="col-start-3 grid grid-cols-2 gap-4">
         <UFormGroup label="Total">
           <UInput placeholder="Total" v-model="total" readonly type="number" />
         </UFormGroup>
@@ -86,12 +133,36 @@
 const dragOptions = computed(() => {
   return { animation: 200, group: "description" };
 });
+
 const componentData = computed(() => {
   return {
     tag: "ul",
     type: "transition-group",
   };
 });
+
+const modifierTypes = ref([
+  {
+    label: "Percentage",
+    icon: "i-material-symbols-percent",
+  },
+  {
+    label: "Multiplier",
+    icon: "i-material-symbols-1x-mobiledata",
+  },
+]);
+
+const selectedModifierType = ref(modifierTypes.value[0]);
+
+const useValueModifier = ref(false);
+watch(
+  useValueModifier,
+  (newValue, oldValue) => {
+    setTotal(calculateTotal());
+    setAverage(calculateAverage());
+  },
+  { deep: true }
+);
 
 const fields = ref([]);
 watch(
@@ -125,11 +196,20 @@ const removeField = (index) => {
   fields.value = fields.value.toSpliced(index, 1);
 };
 
+const recalculateAllProducts = () => {
+  fields.value.forEach((field, index) => calculateProduct(index));
+};
+
 const calculateProduct = (fieldIndex) => {
   const field = fields.value[fieldIndex];
-  field.product = field.multiplier
-    ? field.multiplier * field.value
-    : field.value;
+
+  const multiplier =
+    selectedModifierType.value.label == "Percentage"
+      ? field.multiplier / 100
+      : field.multiplier;
+
+  field.product =
+    useValueModifier && multiplier ? multiplier * field.value : field.value;
   fields.value[fieldIndex] = { ...field };
 };
 
@@ -144,9 +224,7 @@ const calculateTotal = () => {
 
 const calculateAverage = () => {
   const sumOfAllProducts = calculateTotal();
-
   const totalNumberOfProducts = fields.value.length;
-
   const averageBetweenAllProducts = sumOfAllProducts / totalNumberOfProducts;
 
   return averageBetweenAllProducts;
